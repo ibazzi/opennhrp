ibazzi/opennhrp

commit 47d649e6ef15cb35fb5a93bb6d669856a965f85e

admin: prevent flushing of static mappings (when using nbma selector)
errorneously set extra bits in type_mask.

nhrp/admin.c
@@ -167,7 +167,7 @@ static int admin_parse_selector(void *ctx, const char *cmd,
 		} else if (strcmp(keyword, "nbma") == 0) {
 			if (sel->next_hop_address.type != AF_UNSPEC)
 				goto err_conflict;
-			sel->type_mask = ~BIT(NHRP_PEER_TYPE_CACHED_ROUTE);
+			sel->type_mask &= ~BIT(NHRP_PEER_TYPE_CACHED_ROUTE);
 			sel->next_hop_address = address;
 		} else if (strcmp(keyword, "local-protocol") == 0) {
 			if (sel->interface != NULL)

commit b1bfb57abf6bcf8aafb60a31bacd676ac9587d1a

admin: fix 'cache show'
Commit 47d649e6ef15cb35fb5a93bb6d669856a965f85e broke cache show as
type_mask was not explicitly set in cache show. Now set it so
the generic code can just mask out unwanted types.

nhrp/admin.c
@@ -214,6 +214,7 @@ static void admin_cache_show(void *ctx, const char *cmd)
 	struct nhrp_peer_selector sel;
 
 	memset(&sel, 0, sizeof(sel));
+	sel.type_mask = NHRP_PEER_TYPEMASK_ALL;
 	if (!admin_parse_selector(ctx, cmd, &sel))
 		return;
 
@@ -228,7 +229,6 @@ static void admin_cache_purge(void *ctx, const char *cmd)
 
 	memset(&sel, 0, sizeof(sel));
 	sel.type_mask = NHRP_PEER_TYPEMASK_PURGEABLE;
-
 	if (!admin_parse_selector(ctx, cmd, &sel))
 		return;
 
@@ -247,7 +247,6 @@ static void admin_cache_flush(void *ctx, const char *cmd)
 
 	memset(&sel, 0, sizeof(sel));
 	sel.type_mask = NHRP_PEER_TYPEMASK_REMOVABLE;
-
 	if (!admin_parse_selector(ctx, cmd, &sel))
 		return;
 

nhrp/nhrp_peer.h
@@ -45,6 +45,10 @@
 	(NHRP_PEER_TYPEMASK_REMOVABLE | \
 	 BIT(NHRP_PEER_TYPE_STATIC))
 
+#define NHRP_PEER_TYPEMASK_ALL \
+	(NHRP_PEER_TYPEMASK_PURGEABLE | \
+	 BIT(NHRP_PEER_TYPE_LOCAL))
+
 #define NHRP_PEER_FLAG_UNIQUE		0x01	/* Peer is unique; see RFC2332 */
 #define NHRP_PEER_FLAG_REGISTER		0x02	/* For TYPE_STATIC: send registration */
 #define NHRP_PEER_FLAG_CISCO		0x04	/* For TYPE_STATIC: peer is Cisco */